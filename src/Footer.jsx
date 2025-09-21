9d5aefbbe153c88335c48deaa0959c1bac0fd3bf
Uday Kiran <s210540@rguktsklm.ac.in>
2025-09-21 21:09:18 +0530

    login

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="footer sm:footer-horizontall fixed bottom-0 bg-base-300 text-base-content p-4">
+    <footer className="footer sm:footer-horizontal fixed bottom-0 bg-base-300 text-base-content p-4 justify-center">
   <aside>
     <p>Copyright © {new Date().getFullYear()} - All right reserved by ACME Industries Ltd</p>
   </aside>
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="flex justify-center my-10">
+      <div className="card card-border bg-base-300 w-96">
+        <div className="card-body ">
+          <h2 className="card-title justify-center">Login</h2>
+          <div>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Email ID</legend>
+              <input type="text" className="input" placeholder="Type here" />
+            </fieldset>
+          </div>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary">Login </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
