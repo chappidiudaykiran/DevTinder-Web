d84b1a5feb67975c24bcd5e4a1632fbe53913f77
Uday Kiran <s210540@rguktsklm.ac.in>
2025-09-22 22:22:20 +0530

    updated

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [emailid, setEmailid] = useState("");
+  const [Password, setPassword] = useState("");
   return (
     <div className="flex justify-center my-10">
       <div className="card card-border bg-base-300 w-96">
@@ -9,7 +11,25 @@ const Login = () => {
           <div>
             <fieldset className="fieldset">
               <legend className="fieldset-legend">Email ID</legend>
-              <input type="text" className="input" placeholder="Type here" />
+              <input
+                type="text"
+                value={emailid}
+                onChange={(e) => setEmailid(e.target.value)}
+                className="input"
+                placeholder="Type here"
+              />
+            </fieldset>
+          </div>
+          <div>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Password</legend>
+              <input
+                type="text"
+                value={Password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="input"
+                placeholder="Type here"
+              />
             </fieldset>
           </div>
           <div className="card-actions justify-center">
