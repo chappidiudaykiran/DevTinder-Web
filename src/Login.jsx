493e38d0a0abebf67327871fce1a93c1ac30f7ee
Uday Kiran <s210540@rguktsklm.ac.in>
2025-09-30 20:04:52 +0530

    added middleware with credentials

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import axios from "axios";
 const Login = () => {
-  const [emailid, setEmailid] = useState("john@example.com");
-  const [Password, setPassword] = useState("John@123");
+  const [emailid, setEmailid] = useState("Uday@example.com");
+  const [Password, setPassword] = useState("Uday@123");
   const handleLogin = async() => {
     try {
       const response = await axios.post("http://localhost:3000/login", {
-        emailid,
-        Password
+        emailId: emailid,
+        password: Password
+      },{
+        withCredentials: true
       });
       console.log(response.data);
     } catch (error) {
@@ -23,33 +25,35 @@ const Login = () => {
       <div className="card card-border bg-base-300 w-96">
         <div className="card-body ">
           <h2 className="card-title justify-center">Login</h2>
-          <div>
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend">Email ID</legend>
-              <input
-                type="text"
-                value={emailid}
-                onChange={(e) => setEmailid(e.target.value)}
-                className="input"
-                placeholder="Type hhere"
-              />
-            </fieldset>
-          </div>
-          <div>
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend">Password</legend>
-              <input
-                type="text"
-                value={Password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="input"
-                placeholder="Type here"
-              />
-            </fieldset>
-          </div>
-          <div className="card-actions justify-center">
-            <button className="btn btn-primary" onClick={handleLogin}>Login </button>
-          </div>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <fieldset className="fieldset">
+                <legend className="fieldset-legend">Email ID</legend>
+                <input
+                  type="text"
+                  value={emailid}
+                  onChange={(e) => setEmailid(e.target.value)}
+                  className="input"
+                  placeholder="Type here"
+                />
+              </fieldset>
+            </div>
+            <div>
+              <fieldset className="fieldset">
+                <legend className="fieldset-legend">Password</legend>
+                <input
+                  type="password"
+                  value={Password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="input"
+                  placeholder="Type here"
+                />
+              </fieldset>
+            </div>
+            <div className="card-actions justify-center">
+              <button className="btn btn-primary" type="submit">Login</button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
